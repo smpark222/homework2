ca08c130e18958cfa5d5f0c75d2a615c6c892637
smpark222 <sungmin567@naver.com>
2016-09-28 00:55:26 +0900

    1번 문제 B01.java 생성

--- B01.java
+++ B01.java
@@ -1,6 +1,24 @@
+import java.util.*;
 class B01 {
 	public static void main(String[] args){
-		System.out.println("Hello");
-		System.out.println("World");
+		int x = input();
+		System.out.printf("%d",x);
+	}
+
+	public int age(){
+		System.out.printf("태어난 연도를 입력하세요. ");
+		int birth_year = input();
+		int age = 2014 - birth_year + 1;
+	}
+
+	public void result(int age){
+		if(age >= 20) System.out.println("미성년자가 아닙니다.");
+		else System.out.println("미성년자입니다.");
+	}
+
+	public static int input(){
+		Scanner input = new Scanner(System.in);
+		return input.nextInt();
 	}
 }
+

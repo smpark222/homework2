7d98face682e35ca7562cc5adc647550269ae2d0
smpark222 <sungmin567@naver.com>
2016-09-28 01:07:36 +0900

    과제 1번 B01.java 완성

--- B01.java
+++ B01.java
@@ -1,24 +1,24 @@
 import java.util.*;
 class B01 {
-	public static void main(String[] args){
-		int x = input();
-		System.out.printf("%d",x);
-	}
+    public static void main(String[] args){
+        int age = age();
+	result(age);
+    }
 
-	public int age(){
-		System.out.printf("태어난 연도를 입력하세요. ");
-		int birth_year = input();
-		int age = 2014 - birth_year + 1;
-	}
+    public static int age(){
+        System.out.printf("태어난 연도를 입력하세요. ");
+        int birth_year = input();
+        int age = 2014 - birth_year + 1;
+        return age;
+    }
 
-	public void result(int age){
-		if(age >= 20) System.out.println("미성년자가 아닙니다.");
-		else System.out.println("미성년자입니다.");
-	}
+    public static void result(int age){
+        if(age >= 20) System.out.println("미성년자가 아닙니다.");
+        else System.out.println("미성년자입니다.");
+    }
 
-	public static int input(){
-		Scanner input = new Scanner(System.in);
-		return input.nextInt();
-	}
+    public static int input(){
+        Scanner input = new Scanner(System.in);
+        return input.nextInt();
+    }
 }
-
